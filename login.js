308cd878d6ba0e957fb535842ac3e5db60505b0c
Asita Gupta <92207501+asitagupta6@users.noreply.github.com>
2024-02-27 23:56:24 +0530

    login.js

---
+++ login.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement API call to backend for login
+    console.log('Logging in with:', username, password);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
